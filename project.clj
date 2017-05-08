d650de6b8de3a5052a9c83850bf4b554929dcff5
Ryan Anderson <ryan.anderson@ameritas.com>
2017-05-08 13:25:59 -0400

    Added some endpoints

--- project.clj
+++ project.clj
@@ -9,4 +9,5 @@
   :ring {:handler clog.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.0"]]}})
+                        [ring/ring-mock "0.3.0"]
+                        [com.novemberain/monger "3.1.0"]]}})
--- src/clog/handler.clj
+++ src/clog/handler.clj
@@ -3,8 +3,15 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(def route-get-user-posts [userId]
+  (str "All posts for user: " userId))
+
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] "Display all public posts for all users")
+  (GET "/user/:userId/posts/:postId" [] "Specific Post Page")
+  (POST "/user/:userId/posts/" [] "Post a new entry here")
+  (GET "/user/:userId/posts/" [] "Gets all posts for a given user")
+  (DELETE "/user/:userId/posts/:postId" [] "Delete a specific post")
   (route/not-found "Not Found"))
 
 (def app
